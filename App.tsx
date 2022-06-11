e3113293e8be23a7cc4678a6a46186b990f8bfb2
이중민 <boonddiga00@naver.com>
2022-06-11 15:53:54 +0900

    Paint ToDos

--- App.tsx
+++ App.tsx
@@ -6,13 +6,23 @@ import {
   TouchableOpacity,
   View,
   TextInput,
+  ScrollView,
 } from "react-native";
 import { theme } from "./color";
+
+interface IToDos {
+  [key: string]: IToDo;
+}
+
+interface IToDo {
+  text: string;
+  work: boolean;
+}
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
-  const [toDos, setToDos] = useState({});
+  const [toDos, setToDos] = useState<IToDos>({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload: string) => setText(payload);
@@ -58,6 +68,15 @@ export default function App() {
         onChangeText={onChangeText}
         onSubmitEditing={addToDo}
       />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].work === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -83,5 +102,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     borderRadius: 20,
     marginTop: 30,
+    marginBottom: 25,
+  },
+  toDo: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  toDoText: {
+    color: theme.white,
+    fontSize: 18,
   },
 });
